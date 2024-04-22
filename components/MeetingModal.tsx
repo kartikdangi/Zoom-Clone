4334b23c4739170a51b3acab1b91c23d2b8b3683
kartikdangi <kartikdangi26@gmail.com>
2024-04-23 03:24:58 +0530

    Update

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -9,6 +9,10 @@ import {
 	DialogTitle,
 	DialogTrigger,
 } from '@/components/ui/dialog';
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
+import { Title } from '@radix-ui/react-dialog';
+import { Button } from './ui/button';
 
 interface MeetingModalProps {
 	isOpen: boolean;
@@ -21,18 +25,30 @@ interface MeetingModalProps {
 	image?: string;
 	buttonIcon?: string;
 }
-const MeetingModal = ( { isOpen, onClose, title, className, children, handleClick, buttonText, image, buttonIcon}: MeetingModelProps) => {
+const MeetingModal = ( { isOpen, onClose, title, className, children, handleClick, buttonText, image, buttonIcon}: MeetingModalProps) => {
   return (
-	<Dialog open={isOpen} onOpenChange={onClose}>
-  <DialogTrigger>Open</DialogTrigger>
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
-      <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </DialogDescription>
-    </DialogHeader>
+<Dialog open={isOpen} onOpenChange={onClose}>
+  <DialogContent className='flex w-full max--[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white'>
+	<div className='flex flex-col gap-6'>
+		{image && (
+			<div className='flex justify-center'>
+				<Image src={image} alt='image' width={72} height={72} />
+			</div>
+		)}
+	</div>
+	<h1 className={cn('text-3x; font-bold leading-4]', className)}> 
+		{title}
+	</h1>
+	{children}
+	<Button className='bg-blue-1 
+	focus-visible:ring-0 
+	focus:ring-offset-0'
+	onClick={handleClick}>
+		{buttonIcon && (
+			<Image src= {buttonIcon} alt = "button icon" width={13} height={13}/>
+		)} &nbsp;
+		{buttonText || 'Schedule Meeting'}
+	</Button>
   </DialogContent>
 </Dialog>
 
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,19 +3,68 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
 import HomeCard from './HomeCard';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
+import MeetingModal from './MeetingModal';
+import { useUser } from '@clerk/nextjs';
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
+import { useToast } from './ui/use-toast';
 
 const MeetingTypeList = () => {
 const router = useRouter();
-const createMeeting = () => {};
+const { user } = useUser();
+const client = useStreamVideoClient();
+const [values, setValues] = useState({
+	dateTime: new Date(),
+	description: '',
+	link: '',
+});
+
+const { toast } = useToast();
+
+const [callDetails, setCallDetails] = useState<Call>();
+
+const createMeeting = async () => {
+	if (!client || !user) return;
+
+	try {
+		const id = crypto.randomUUID();
+		const call = client.call('default',  id);
+		if (!call) throw new Error('Failed to create call');
+
+		const startsAt  =values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+		const description = values.description || 'Instant Meeting';
+		
+		await call.getOrCreate({
+			data: {
+				starts_at: startsAt,
+				custom: {
+					description
+				}
+			}
+		})
+
+		setCallDetails(call);
+
+		if(!values.description) {
+			router.push(`/meeting/${call.id}`)
+		}
+		
+	} catch (error) {
+		console.log(error);
+		toast({
+			title: 'Scheduled: Catch up',
+			description: 'Friday, february 10, 2024',
+		})
+	}
+};
  const [meetingState, setMeetingState] = useState<'isSchedulingMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>(); 
  return (
 	<section className='grid grid-cols-1 gap-5 md:grid grid-cols-2 xl: grid-cols-4'>
 	  <HomeCard 
 		  img="/icons/add-meeting.svg"
-		  title="New Meetng"
+		  title="New Meeting"
 		  description="Start an Instant Meeting"
-		  handleClick={() => setMeetingState('isJoiningMeeting')}	
+		  handleClick={() => setMeetingState('isInstantMeeting')}	
 		  className="bg-orange-1"
 	  	/>
 
